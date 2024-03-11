5655610c55e22622245e4af79ff66711f525f408
Patrick Selin <patrick.selin@gmail.com>
2024-03-11 11:14:32 +0200

    11.6 fix code

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const express = require("express");
-const app = express();
+const express = require('express')
+const app = express()
 
 // get the port from env variable
-const PORT = process.env.PORT || 5005;
+const PORT = process.env.PORT || 5005
 
-app.use(express.static("dist"));
+app.use(express.static('dist'))
 
 app.listen(PORT, () => {
-  console.log(`Server started on port: ${PORT}`);
-});
+  console.log(`Server started on port: ${PORT}`)
+})
