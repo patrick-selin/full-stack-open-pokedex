0094e1e4520c1ed481f5efb11d1330b7bf0312c9
Patrick Selin <patrick.selin@gmail.com>
2024-03-11 15:34:02 +0200

    11.9 Simple end to end test, fex

--- cypress/e2e/pokemon.cy.js
+++ cypress/e2e/pokemon.cy.js
@@ -1,6 +1,6 @@
 describe('Pokedex', function () {
   it('front page can be opened', function () {
-    cy.visit('http://localhost:8080')
+    cy.visit('http://localhost:5005')
     cy.contains('ivysaur')
     cy.contains(
       'Pokémon and Pokémon character names are trademarks of Nintendo.'
@@ -8,7 +8,7 @@ describe('Pokedex', function () {
   })
 
   it('user can go from the front page to the right Pokemon page', function() {
-    cy.visit('http://localhost:8080')
+    cy.visit('http://localhost:5005')
     cy.contains('ivysaur').click()
     cy.url().should('include', '/pokemon/ivysaur')
     cy.contains('ivysaur').should('exist')
